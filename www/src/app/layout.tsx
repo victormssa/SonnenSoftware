26eef6092b078f108586abad6ec93302a0451209
Victor Alves <victormssalves@outlook.com>
2023-12-27 14:31:46 -0300

    Retirando SEO

--- www/src/app/layout.tsx
+++ www/src/app/layout.tsx
@@ -5,7 +5,6 @@ import Header from '@/components/header/Header';
 import Footer from '@/components/footer/Footer';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import {DefaultSeo} from 'next-seo';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,29 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>    <DefaultSeo
-  title="Sonnen Software"
-  description="SonnenSoftware - Desenvolvendo Soluções Digitais! 🚀🌐 Inovação, elegância e funcionalidade para marcar sua presença online. 💻✨"
-  openGraph={{
-    type: 'website',
-    locale: 'en_IE',
-    url: 'https://sonnensoftware.com/',
-    siteName: 'Sonnen Software',
-    images: [
-      {
-        url: 'https://raw.githubusercontent.com/victormssa/SonnenSoftware/main/www/public/assets/images/logoSonnen.png',
-        width: 1200,
-        height: 630,
-        alt: 'Texto Alternativo da Imagem',
-      },
-    ],
-  }}
-  twitter={{
-    handle: '@handle',
-    site: '@sonnensoftware',
-    cardType: 'summary_large_image',
-  }}
-/><Header/>{children}<Footer/><Analytics /><SpeedInsights /></body>
+      <body className={inter.className}><Header/>{children}<Footer/><Analytics /><SpeedInsights /></body>
     </html>
   )
 }
