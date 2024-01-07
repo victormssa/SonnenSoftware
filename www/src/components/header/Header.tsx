7ffd872cc9c09a14fc71ee9324d45b4681d313de
Victor Alves <victormssalves@outlook.com>
2024-01-07 16:12:00 -0300

    Resolvendo de verdade k

--- www/src/components/header/Header.tsx
+++ www/src/components/header/Header.tsx
@@ -41,9 +41,7 @@ const Navigation = () => {
   const specialPages = ["/login", "/contato", "/politicas/privacidade"];
   const isSpecialPage = specialPages.includes(pathname);
   const isActive = (path: string) => {
-    return pathname === path
-      ? " text-red-600"
-      : "";
+    return pathname === path ? " text-red-600" : "";
   };
 
   const isActive2 = (path: string) => {
@@ -55,23 +53,29 @@ const Navigation = () => {
   return (
     <nav
       className={`fixed w-full z-20 transition-all ${
-        isTop && !isSpecialPage && !isOpen ? "bg-transparent" : "shadow bg-gradient-to-tr from-red-600 to-rose-800"
+        isTop && !isSpecialPage && !isOpen
+          ? "bg-transparent"
+          : "shadow bg-gradient-to-tr from-red-600 to-rose-800"
       }`}
     >
       <div className=" px-6 py-4 mx-auto">
         <div className="lg:flex lg:items-center lg:justify-between">
           <div className="flex items-center justify-between">
-            <Link href="/" className="flex items-center justify-between lg:ml-20 ml-0">
-            <Image
-              className={`w-40 h-auto transition-all `}
-              src={isTop && !isSpecialPage ?  headerLogoBlack : headerLogoBlack}
-              alt=""
-            />
+            <Link
+              href="/"
+              className="flex items-center justify-between lg:ml-20 ml-0"
+            >
+              <Image
+                className={`w-40 h-auto transition-all `}
+                src={
+                  isTop && !isSpecialPage ? headerLogoBlack : headerLogoBlack
+                }
+                alt=""
+              />
             </Link>
 
             {/* Mobile menu button */}
             <div className="flex lg:hidden">
-             
               <button
                 onClick={toggleMenu}
                 type="button"
@@ -120,51 +124,97 @@ const Navigation = () => {
             }`}
           >
             <div className="flex flex-col -mx-6 lg:flex-row lg:items-center lg:mx-8">
-            <Link
+              <Link
                 href="/#planos"
                 className={`px-3 py-2 duration-200 hover:text-black text-white font-semibold mr-4 lg:border-y-0 border-y-2 lg:w-auto w-screen text-center ${isActive(
                   "/#planos"
                 )} ${
-                  isTop && !isSpecialPage ?  "lg:hover:text-red-600 hover:text-rose-300 " : " hover:text-rose-300"
+                  isTop && !isSpecialPage
+                    ? "lg:hover:text-red-600 hover:text-rose-300 "
+                    : " hover:text-rose-300"
                 }`}
               >
                 Nossos Planos
               </Link>
               <div className="flex flex-col">
                 <button
-                
                   className={`px-3 py-2 duration-200 hover:text-black text-white font-semibold mr-4 lg:border-y-0 border-b-2 lg:w-auto w-screen flex items-center gap-1 justify-center ${isActive(
                     "/#planos"
                   )} ${
-                    isTop && !isSpecialPage ?  "lg:hover:text-red-600 hover:text-rose-300 " : " hover:text-rose-300"
-                  }`} onClick={toggleLista}
+                    isTop && !isSpecialPage
+                      ? "lg:hover:text-red-600 hover:text-rose-300 "
+                      : " hover:text-rose-300"
+                  }`}
+                  onClick={toggleLista}
                 >
-                  Nosso Trabalho {mostrarLista ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
+                  Nosso Trabalho{" "}
+                  {mostrarLista ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
                 </button>
                 {mostrarLista && (
-                        <div className="lg:absolute top-[2rem] left-[11.5rem] flex flex-col lg:px-2 lg:py-2 lg:rounded-xl rounded-none lg:gap-3 lg:border-b-0 border-b-2 lg:bg-transparent bg-white
-                        ">
-                          <Link className={`lg:bg-red-600 lg:hover:bg-red-500 lg:text-white text-black py-1 px-2 lg:w-40 lg:rounded-lg lg:border-b-0 border-b-2 text-center ${isActive(
-                  "/sobre/desenvolvimento_sites"
-                )}`} href="/sobre/desenvolvimento_sites" onClick={toggleMenu}>Desenvolvimento de Sites</Link>
-                          <Link className={`lg:bg-red-600 lg:hover:bg-red-500 lg:text-white text-black py-1 px-2 lg:w-40 lg:rounded-lg lg:border-b-0 border-b-2 text-center ${isActive(
-                  "/sobre/desenvolvimento_apps"
-                )}`} href="/sobre/desenvolvimento_apps" onClick={toggleMenu}>Desenvolvimento de Apps</Link>
-                          <Link className={`lg:bg-red-600 lg:hover:bg-red-500 lg:text-white text-black py-1 px-2 lg:w-40 lg:rounded-lg lg:border-b-0 border-b-2 text-center ${isActive(
-                  "/sobre/otimizacao_sistemas"
-                )}`} href="/sobre/otimizacao_sistemas" onClick={toggleMenu}>Otimização de Sistemas</Link>
-                          <Link className={`lg:bg-red-600 lg:hover:bg-red-500 lg:text-white text-black py-1 px-2 lg:w-40 lg:rounded-lg text-center ${isActive(
-                  "/sobre/solucoes_personalizadas"
-                )}`} href="/sobre/solucoes_personalizadas" onClick={toggleMenu}>Soluções Personalizadas</Link>
-                        </div>
-                      )}
+                  <div
+                    className="lg:absolute top-[2rem] left-[11.5rem] flex flex-col lg:px-2 lg:py-2 lg:rounded-xl rounded-none lg:gap-3 lg:border-b-0 border-b-2 lg:bg-transparent bg-white
+                        "
+                  >
+                    <Link
+                      className={`lg:bg-red-600 lg:hover:bg-red-500 lg:text-white text-black py-1 px-2 lg:w-40 lg:rounded-lg lg:border-b-0 border-b-2 text-center ${isActive(
+                        "/sobre/desenvolvimento_sites"
+                      )}`}
+                      href="/sobre/desenvolvimento_sites"
+                      onClick={() => {
+                        toggleMenu();
+                        toggleLista();
+                      }}
+                    >
+                      Desenvolvimento de Sites
+                    </Link>
+
+                    <Link
+                      className={`lg:bg-red-600 lg:hover:bg-red-500 lg:text-white text-black py-1 px-2 lg:w-40 lg:rounded-lg lg:border-b-0 border-b-2 text-center ${isActive(
+                        "/sobre/desenvolvimento_apps"
+                      )}`}
+                      href="/sobre/desenvolvimento_apps"
+                      onClick={() => {
+                        toggleMenu();
+                        toggleLista();
+                      }}
+                    >
+                      Desenvolvimento de Apps
+                    </Link>
+                    <Link
+                      className={`lg:bg-red-600 lg:hover:bg-red-500 lg:text-white text-black py-1 px-2 lg:w-40 lg:rounded-lg lg:border-b-0 border-b-2 text-center ${isActive(
+                        "/sobre/otimizacao_sistemas"
+                      )}`}
+                      href="/sobre/otimizacao_sistemas"
+                      onClick={() => {
+                        toggleMenu();
+                        toggleLista();
+                      }}
+                    >
+                      Otimização de Sistemas
+                    </Link>
+                    <Link
+                      className={`lg:bg-red-600 lg:hover:bg-red-500 lg:text-white text-black py-1 px-2 lg:w-40 lg:rounded-lg text-center ${isActive(
+                        "/sobre/solucoes_personalizadas"
+                      )}`}
+                      href="/sobre/solucoes_personalizadas"
+                      onClick={() => {
+                        toggleMenu();
+                        toggleLista();
+                      }}
+                    >
+                      Soluções Personalizadas
+                    </Link>
+                  </div>
+                )}
               </div>
               <Link
                 href="/contato"
                 className={`px-3 py-2 duration-200 text-white hover:text-black font-semibold lg:border-2 border-b-2 lg:w-auto w-screen mr-4 text-center ${isActive2(
                   "/contato"
                 )} ${
-                  isTop && !isSpecialPage ?  "lg:hover:bg-red-600 lg:border-red-600 lg:hover:text-black hover:text-rose-300" : "border-white lg:hover:bg-white lg:hover:text-red-600 hover:text-rose-300"
+                  isTop && !isSpecialPage
+                    ? "lg:hover:bg-red-600 lg:border-red-600 lg:hover:text-black hover:text-rose-300"
+                    : "border-white lg:hover:bg-white lg:hover:text-red-600 hover:text-rose-300"
                 }`}
               >
                 Solicite um orçamento
@@ -173,26 +223,26 @@ const Navigation = () => {
                 className={`md:hidden lg:hidden 
                     ml-10 w-[4.8rem] bg-gray-700 lg:mr-10 sm:mr-0 lg:pr-4 lg:border-2 border-red-500 dark:border-red-500 rounded-full
                 `}
-              >
-              </div>
+              ></div>
               <Link
                 href="/login"
                 className={`px-3 py-2 duration-200 sm:hover:bg-none text-white hover:text-black font-semibold lg:border-2 border-b-2 lg:rounded-lg rounded-none lg:border-b-2 lg:w-auto w-screen text-center ${isActive2(
                   "/login"
                 )} ${
-                  isTop && !isSpecialPage ?  "lg:hover:bg-red-600 lg:border-red-600 lg:hover:text-black hover:text-rose-300" : "border-white lg:hover:bg-white lg:hover:text-red-600 hover:text-rose-300"
+                  isTop && !isSpecialPage
+                    ? "lg:hover:bg-red-600 lg:border-red-600 lg:hover:text-black hover:text-rose-300"
+                    : "border-white lg:hover:bg-white lg:hover:text-red-600 hover:text-rose-300"
                 }`}
               >
                 Login
               </Link>
-              
             </div>
             <p
-                
-                className={`lg:text-2xl px-3 py-2 duration-200 font-medium mr-4 lg:border-y-0 border-y-2 lg:w-auto w-screen text-white lg:ml-20 lg:flex items-center gap-2  hidden`}
-              >
-                <BiSolidPhoneCall />(71) 9961-84966
-              </p>
+              className={`lg:text-2xl px-3 py-2 duration-200 font-medium mr-4 lg:border-y-0 border-y-2 lg:w-auto w-screen text-white lg:ml-20 lg:flex items-center gap-2  hidden`}
+            >
+              <BiSolidPhoneCall />
+              (71) 9961-84966
+            </p>
           </div>
         </div>
       </div>
