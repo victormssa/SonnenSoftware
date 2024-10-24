41cbf18af7ea856b36c27a96ebe4b63c43a78a8d
Victor Alves <victormssalves@outlook.com>
2024-10-23 22:58:58 -0300

    Clarity

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -77,6 +77,15 @@ export default function RootLayout({
       gtag('config', 'G-XBQWCCGZ4V');
     `}
           </Script>
+          <Script id="clarity-analytics" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "onevjwgns6");
+        `}
+      </Script>
         </head>
         <body className={poppins.className}>
           <CookieBanner />
